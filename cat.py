c30fe37748881565a8e78b5e1e812899b91af676
Stocco <renato_stoco@hotmail.com>
2016-03-03 14:19:22 -0800

    Added support vector machines training using scikit. And some dictionary helper functions

--- cat.py
+++ cat.py
@@ -1,12 +1,17 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
-
 import math
 import random
 import requests
 from bs4 import BeautifulSoup
 import re
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDClassifier
+import numpy as np
+from sklearn import metrics
 tokenizer = RegexpTokenizer(r'\w+')
 #globals DONT TOUCH THEM
 path = "train.txt"
@@ -90,6 +95,31 @@ def binning(string):
         "talk.religion.misc" : 1
     }
     return interest[string]
+
+def LabeltoNum(label):
+    interest={
+        "alt.atheism" : 0,
+        "comp.graphics" : 1,
+        "comp.os.ms-windows.misc" : 2,
+        "comp.sys.ibm.pc.hardware" : 3,
+        "comp.sys.mac.hardware" : 4,
+        "comp.windows.x" : 5,
+        "misc.forsale" : 6,
+        "rec.autos" : 7,
+        "rec.motorcycles" : 8,
+        "rec.sport.baseball" : 9,
+        "rec.sport.hockey" : 10,
+        "sci.crypt" : 11,
+        "sci.electronics"	: 12,
+        "sci.med"	: 13,
+        "sci.space" : 14,
+        "soc.religion.christian" : 15,
+        "talk.politics.guns" : 16,
+        "talk.politics.mideast" : 17,
+        "talk.politics.misc" : 18,
+        "talk.religion.misc" : 19
+    }
+    return int(interest[label])
 
 def loadInterest(path):
     interestClass={1:0,2:0,3:0,4:0}
@@ -203,12 +233,61 @@ def main():
     output.write(str(i[0])+" category: "+str(i[1])+"\n")
   output.close()
 
+def svmToolkitTrain():
+  docs = open(path).read().split("\n")
+  docstest = open(pathtest).read().split("\n")
+  text_clf = Pipeline([('vect', CountVectorizer()),
+                      ('tfidf', TfidfTransformer()),
+                      ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                            alpha=1e-3, n_iter=5, random_state=42)),
+ ])
+  class dados(object):
+      data = []
+      target = []
+      target_names=['alt.atheism','comp.graphics','comp.os.ms-windows.misc','comp.sys.ibm.pc.hardware',
+                    'comp.sys.mac.hardware','comp.windows.x','misc.forsale','rec.autos','rec.motorcycles',
+                    'rec.sport.baseball','rec.sport.hockey','sci.crypt','sci.electronics','sci.med',
+                    'sci.space','soc.religion.christian','talk.politics.guns','talk.politics.mideast','talk.politics.misc',
+                    'talk.religion.misc']
+  count_vect = CountVectorizer()
+  train = dados()
+  test =dados()
+  del docs[len(docs)-1]
+  for i in docs:
+      doc = i.split("\t")
+      train.data.append(doc[1])
+      train.target.append(LabeltoNum(doc[0]))
+
+  del docstest[len(docstest)-1]
+  for i in docstest:
+      doc = i.split("\t")
+      test.data.append(doc[1])
+      test.target.append(LabeltoNum(doc[0]))
+
+  clf = text_clf.fit(train.data,train.target)
+
+  x = clf.predict([test.data[15]])
+  print(test.target_names[x], "ca", test.data[15])
+
+
+  docs_eval = test.data
+  predicted = clf.predict(docs_eval)
+  print(metrics.classification_report(test.target, predicted,
+      target_names=test.target_names))
+
+  return clf,train.target_names
 
 
 #Initializing Variables using Training set
 #classes,totaldocs,docs = loadtrain(path)
 #Voc = float(farmingvoc(classes))
 #main()
+#loadInterest("Links.txt")
 
-loadInterest("Links.txt")
+
+
+# scrapped
+#  twenty_train = fetch_20newsgroups(subset='train',shuffle='True',random_state=42)
+
+
 
